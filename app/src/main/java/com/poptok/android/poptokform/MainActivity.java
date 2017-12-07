f546d58b2cc733ae5ae739b305105e1930c77d26
YeonJin <duswls0515@gmail.com>
2017-12-07 15:15:36 +0900

    Initial commit

---
+++ app/src/main/java/com/poptok/android/poptokform/MainActivity.java
@@ -0,0 +1,20 @@
+package com.poptok.android.poptokform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BIT on 2017-12-07.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
